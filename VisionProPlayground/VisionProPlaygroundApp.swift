c40dd2151128eb1c857a1a53a60505bdafd1715b
Norbu Sonam <43245807+norbusonam@users.noreply.github.com>
2024-03-09 10:59:46 -0800

    Initial Commit

---
+++ VisionProPlayground/VisionProPlaygroundApp.swift
@@ -0,0 +1,17 @@
+//
+//  VisionProPlaygroundApp.swift
+//  VisionProPlayground
+//
+//  Created by Norbu Sonam on 3/9/24.
+//
+
+import SwiftUI
+
+@main
+struct VisionProPlaygroundApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
