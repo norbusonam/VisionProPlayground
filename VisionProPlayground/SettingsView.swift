a6b8391918260ce86dfda64e042bb61690c68f05
Norbu Sonam <43245807+norbusonam@users.noreply.github.com>
2024-03-14 09:15:13 -0700

    sheets in vision os

--- VisionProPlayground/SettingsView.swift
+++ VisionProPlayground/SettingsView.swift
@@ -8,8 +8,18 @@
 import SwiftUI
 
 struct SettingsView: View {
+    @State private var isSheetOpen = false
     var body: some View {
-        Text("Settings")
+        VStack {
+            Button("Open Sheet") {
+                isSheetOpen = true
+            }
+        }
+        .sheet(isPresented: $isSheetOpen, content: {
+            Button("Close Sheet") {
+                isSheetOpen = false
+            }
+        })
     }
 }
 
