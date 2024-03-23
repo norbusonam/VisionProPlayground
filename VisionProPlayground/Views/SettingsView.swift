1ed09e50c32e1de497a5a4d0089210e8fbaa4337
Norbu Sonam <43245807+norbusonam@users.noreply.github.com>
2024-03-22 21:51:34 -0700

    navigation split view

--- VisionProPlayground/Views/SettingsView.swift
+++ VisionProPlayground/Views/SettingsView.swift
@@ -7,10 +7,33 @@
 
 import SwiftUI
 
+struct SettingPage: Identifiable {
+    var id = UUID()
+    var name: String
+}
+
+let pages = [SettingPage(name: "Profile"), SettingPage(name: "Appearance"), SettingPage(name: "Privacy")]
+
 struct SettingsView: View {
+    @State private var selectedPageId = pages.first?.id
     
     var body: some View {
-        Text("Settings")
+        NavigationSplitView {
+            List(pages, selection: $selectedPageId) { page in
+                Text(page.name)
+            }
+            .navigationTitle("Settings")
+        } detail: {
+            if let selectedPageId,
+               let page = pages.first(where: { sp in
+                   sp.id == selectedPageId
+               }) {
+                Text(page.name)
+                    .navigationTitle(page.name)
+            } else {
+                Text("Select a settings page")
+            }
+        }
     }
 }
 
